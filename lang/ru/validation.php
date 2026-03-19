b2daa139a7c35bf001c7b6621f44525317120935
Манукьян Елена Михайловна <e.manukyan@aitiguru.com>
2026-03-19 21:08:42 +0300

    fix: correct locales for hexlet ci integrations

--- lang/ru/validation.php
+++ lang/ru/validation.php
@@ -118,7 +118,7 @@ return [
     'prohibited_unless'    => 'Поле :attribute запрещено, если :other не входит в :values.',
     'prohibits'            => 'Поле :attribute запрещает присутствие :other.',
     'regex'                => 'Формат поля :attribute некорректен.',
-    'required'             => 'Поле :attribute обязательно для заполнения.',
+    'required'             => 'Это обязательное поле.',
     'required_array_keys'  => 'Поле :attribute должно содержать значения для: :values.',
     'required_if'          => 'Поле :attribute обязательно для заполнения, когда :other равно :value.',
     'required_if_accepted' => 'Поле :attribute обязательно, когда :other принято.',
