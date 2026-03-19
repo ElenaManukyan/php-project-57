edeb246a87a3bb1485632aee313c6d64e3709303
Манукьян Елена Михайловна <e.manukyan@aitiguru.com>
2026-03-19 22:53:54 +0300

    fix: correct code for tests

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -22,11 +22,11 @@ class TaskController extends Controller
         /** @var QueryBuilder $tasks */
         $tasks = QueryBuilder::for(Task::class)
             ->with(['status', 'author', 'assignee'])
-            ->allowedFilters([
+            ->allowedFilters(
                 AllowedFilter::exact('status_id'),
                 AllowedFilter::exact('created_by_id'),
                 AllowedFilter::exact('assigned_to_id')
-            ])
+            )
             ->orderBy('id', 'asc')
             ->paginate(15);
 
